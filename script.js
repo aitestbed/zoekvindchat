87450a03afc50598e1a8192a2d4d85cdf1841059
aitestbed <128209439+aitestbed@users.noreply.github.com>
2023-03-29 15:37:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const api_url = "https://cors-anywhere.herokuapp.com/https://zoeken.oba.nl/api/v1/search/?q=1984&authorization=76f45dfa187d66be5fd6af05573eab04&output=json";
+const api_url_base = "https://cors-anywhere.herokuapp.com/https://zoeken.oba.nl/api/v1/search/?q=";
+const api_key = "authorization=76f45dfa187d66be5fd6af05573eab04";
+const api_output = "&output=json";
 
-async function getPlaatjes() {
+async function getResults(searchTerm) {
+    const api_url = api_url_base + searchTerm + "&" + api_key + api_output;
     try {
         const response = await fetch(api_url);
         const data = await response.json();
         const results = data.results;
 
-        const plaatjesContainer = document.getElementById("plaatjesContainer");
+        const resultsContainer = document.getElementById("resultsContainer");
+        resultsContainer.innerHTML = ""; // Clear previous results
 
         results.forEach(result => {
             const plaatje = document.createElement("img");
             plaatje.src = result.coverimages[0];
             plaatje.alt = result.titles[0];
             plaatje.style.margin = "10px";
-            plaatjesContainer.appendChild(plaatje);
+            resultsContainer.appendChild(plaatje);
+
+            const titleLink = document.createElement("a");
+            titleLink.href = result.detaillink;
+            titleLink.innerText = result.titles[0];
+            titleLink.style.display = "block";
+            resultsContainer.appendChild(titleLink);
         });
     } catch (error) {
         console.error("Error fetching data:", error);
     }
 }
 
-document.addEventListener("DOMContentLoaded", getPlaatjes);
+function search() {
+    const searchTerm = document.getElementById("searchTerm").value;
+    getResults(searchTerm);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    getResults("1984");
+});
