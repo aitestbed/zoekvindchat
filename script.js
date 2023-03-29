b5886041e832bb8f239fa17409bac51b034c5b97
aitestbed <128209439+aitestbed@users.noreply.github.com>
2023-03-29 13:23:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 const api_url = "https://zoeken.oba.nl/api/v1/search/?q=1984&authorization=76f45dfa187d66be5fd6af05573eab04&output=json";
-const cors_proxy_url = "http://localhost:8080/";
 
-async function getBookData() {
+async function getJSON() {
     try {
-        const response = await fetch(cors_proxy_url + api_url, {
-            method: "GET",
-            headers: {
-                "Origin": "",
-            },
-        });
+        const response = await fetch(api_url);
         const data = await response.json();
-        const result = data.results[0];
 
-        const titel = document.getElementById("titel");
-        const plaatje = document.getElementById("plaatje");
-        const link = document.getElementById("link");
-
-        titel.innerText = result.titles[0].title;
-        plaatje.src = result.coverimages[0].coverimage;
-        link.href = result.detaillink;
+        const jsonOutput = document.getElementById("jsonOutput");
+        jsonOutput.innerText = JSON.stringify(data, null, 2);
     } catch (error) {
         console.error("Error fetching data:", error);
     }
 }
 
-getBookData();
+getJSON();
