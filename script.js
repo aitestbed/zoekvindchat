0f50f9db75ff3e39f749ed43dabf6af04700d715
aitestbed <128209439+aitestbed@users.noreply.github.com>
2023-03-29 09:47:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const api_url = "https://zoeken.oba.nl/api/v1/search/?q=1984&authorization=76f45dfa187d66be5fd6af05573eab04&output=json";
+
+async function getBookData() {
+    try {
+        const response = await fetch(api_url);
+        const data = await response.json();
+        const result = data.results[0];
+
+        const titel = document.getElementById("titel");
+        const plaatje = document.getElementById("plaatje");
+        const link = document.getElementById("link");
+
+        titel.innerText = result.titles[0].title;
+        plaatje.src = result.coverimages[0].coverimage;
+        link.href = result.detaillink;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+getBookData();
