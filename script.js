70ac89221057bac511ac1dbc13376729921b6e9a
aitestbed <128209439+aitestbed@users.noreply.github.com>
2023-03-30 15:02:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const api_url_base = "https://cors-anywhere.herokuapp.com/https://zoeken.oba.nl/api/v1/search/?q=";
-const api_key = "authorization=76f45dfa187d66be5fd6af05573eab04";
-const api_output = "&output=json";
+async function getResults(searchTerm, category) {
+  const api_url_base = "https://cors-anywhere.herokuapp.com/https://zoeken.oba.nl/api/v1/search/?q=";
+  const api_key = "authorization=76f45dfa187d66be5fd6af05573eab04";
+  const api_output = "&output=json";
+  let api_facet = "";
 
-async function getResults(searchTerm, facet = "") {
-  const api_url = api_url_base + searchTerm + facet + "&" + api_key + api_output;
+  if (category === "boeken") {
+    api_facet = "&facet=Type(book)";
+  } else if (category === "dvds") {
+    api_facet = "&facet=Type(movie)";
+  } else if (category === "activiteiten") {
+    api_facet = "%20table:Activiteiten";
+  } else if (category === "cursussen") {
+    api_facet = "%20table:jsonsrc";
+  }
+
+  const api_url = api_url_base + searchTerm + api_facet + "&" + api_key + api_output;
 
   try {
     const response = await fetch(api_url, {
@@ -25,8 +36,44 @@ async function getResults(searchTerm, facet = "") {
   }
 }
 
-function showResults(category, results) {
-  const resultsContainer = document.getElementById(category + "Results");
+async function search() {
+  const searchTerm = document.getElementById("searchTerm").value.trim();
+
+  if (searchTerm.length === 0) {
+    return;
+  }
+
+  const categories = [
+    { name: "boeken", container: "boekenContainer" },
+    { name: "dvds", container: "dvdsContainer" },
+    { name: "activiteiten", container: "activiteitenContainer" },
+    { name: "cursussen", container: "cursussenContainer" },
+  ];
+
+  for (const category of categories) {
+    const results = await getResults(searchTerm, category.name);
+
+    if (results.length > 0) {
+      showResults(category.container, results);
+      document.getElementById(category.container).style.display = "block";
+    } else {
+      document.getElementById(category.container).style.display = "none";
+    }
+  }
+}
+
+document.getElementById("searchForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  search();
+});
+
+const categoryContainers = document.querySelectorAll(".category-container");
+categoryContainers.forEach((container) => {
+  container.style.display = "none";
+});
+
+function showResults(containerId, results) {
+  const resultsContainer = document.getElementById(containerId).querySelector(".results-container");
   resultsContainer.innerHTML = "";
 
   results.forEach((result) => {
@@ -51,43 +98,3 @@ function showResults(category, results) {
     resultsContainer.appendChild(item);
   });
 }
-
-async function search() {
-  const searchTerm = document.getElementById("searchTerm").value.trim();
-
-  if (searchTerm.length === 0) {
-    return;
-  }
-
-  categoryContainers.forEach((container) => {
-    container.style.display = "none";
-  });
-
-  const categories = [
-    { name: "boeken", facet: "&facet=type(book)" },
-    { name: "dvds", facet: "&facet=type(movie)" },
-    { name: "activiteiten", facet: "%20table:Activiteiten" },
-    { name: "cursussen", facet: "%20table:jsonsrc" },
-  ];
-
-  for (const category of categories) {
-    const results = await getResults(searchTerm, category.facet);
-    if (results.length > 0) {
-      showResults(category.name, results);
-      document.getElementById(category.name + "Container").style.display = "block";
-    } else {
-      document.getElementById(category.name + "Container").style.display = "none";
-    }
-  }
-}
-
-
-document.getElementById("searchForm").addEventListener("submit", (event) => {
-  event.preventDefault();
-  search();
-});
-
-const categoryContainers = document.querySelectorAll(".category-container");
-categoryContainers.forEach((container) => {
-  container.style.display = "none";
-});
